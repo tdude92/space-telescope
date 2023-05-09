e84b0848014fd2b21f517da35ac5f895f8765734
Trevor Du <trevorguanhaodu@gmail.com>
2023-05-09 02:50:44 -0400

    try to setup db

---
+++ migrations/09-05-2023_create_render_jobs_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE render_jobs(
+    id uuid NOT NULL,
+    PRIMARY KEY (id),
+    created_at timestamp NOT NULL,
+    fov_x real NOT NULL,
+    fov_y real NOT NULL,
+    image_dimension_x integer NOT NULL,
+    image_dimension_y integer NOT NULL,
+    fundamental_plane_bases real[2][3] NOT NULL,
+    primary_direction real[3] NOT NULL,
+    observer_position real[3] NOT NULL,
+    latitude real NOT NULL,
+    longitude real NOT NULL,
+    broadband_filters text[],
+    narrowband_filters real[],
+);
